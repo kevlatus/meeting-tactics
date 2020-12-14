fd99cacdde41e66e6ae234fb9c51f3db6a53eae4
Kevin Latusinski <me@kevlatus.de>
2020-12-14 19:32:04 +0100

    fix: make footer text adjust to theme color

--- lib/widgets/layout/app_footer.dart
+++ lib/widgets/layout/app_footer.dart
@@ -9,37 +9,37 @@ class AppFooter extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
       color: Theme.of(context).colorScheme.surface,
-      child: DefaultTextStyle(
-        style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              child: Wrap(
-                spacing: 8.0,
-                runSpacing: 8.0,
-                children: [
-                  // HyperLink(
-                  //   href: '/privacy',
-                  //   color: Colors.black,
-                  //   child: Text('Privacy Policy'),
-                  // ),
-                  // HyperLink(
-                  //   href: '/legal',
-                  //   color: Colors.black,
-                  //   child: Text('Legal Notice'),
-                  // ),
-                ],
-              ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: Wrap(
+              spacing: 8.0,
+              runSpacing: 8.0,
+              children: [
+                // HyperLink(
+                //   href: '/privacy',
+                //   color: Colors.black,
+                //   child: Text('Privacy Policy'),
+                // ),
+                // HyperLink(
+                //   href: '/legal',
+                //   color: Colors.black,
+                //   child: Text('Legal Notice'),
+                // ),
+              ],
             ),
-            HyperLink(
-              href: 'https://kevlatus.de',
-              color: Colors.black,
-              decoration: TextDecoration.none,
+          ),
+          HyperLink(
+            href: 'https://kevlatus.de',
+            color: Colors.black,
+            decoration: TextDecoration.none,
+            child: DefaultTextStyle(
+              style: TextStyle(color: Theme.of(context).colorScheme.onSurface),
               child: Text('Made with ♥ by kevlatus'),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
